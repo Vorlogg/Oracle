401ab5573e25317cae610823e42ab47cac70b2c3
Vorlogg <vorlog03@gmail.com>
2021-06-04 17:57:55 +0300

    1.0

--- oracle.py
+++ oracle.py
@@ -1,29 +1,8 @@
-from sqlalchemy import create_engine
 from pymongo import MongoClient
 import random
-import csv
-import multiprocessing
-#
-# # Подключение к серверу Oracle на локальном хосте с помощью cx-Oracle DBAPI.
-# engine = create_engine("oracle+cx_oracle://olaptrain:qwe123@localhost:1521/pdb1.localdomain")
-# engine.connect()
-# print(engine)
-
-# //localhost:1521/pdb1.localdomain
+from tqdm import tqdm
 
 import cx_Oracle
-
-
-# connection = cx_Oracle.connect(
-# "sys",
-# "oracle",
-# "itmo.fatalist.tech:1522/xe",
-# cx_Oracle.SYSDBA
-# )
-#
-# cursor = connection.cursor()
-# print(cursor)
-
 
 def add_student(id: int, name: str, home: int, nomer: int, floop: int) -> dict:
     """Генерация студента"""
@@ -76,12 +55,13 @@ def add_hostel(db, HOSTEL_ID: int, LOCATION: str, ROOM_COUNT: int):
 
 # INSERT INTO AWM.ROOM (ROOM_ID, "number", TYPE_ROOM, RESIDENTS, DISINFECTION, BEDBUG, WARNING)
 # VALUES (2, 3, '2 fg', 2, TO_DATE('2020-05-31 19:23:55', 'YYYY-MM-DD HH24:MI:SS'), 1, 2);
-def add_room(db, ROOM_ID: int, NUMBER1: int, TYPE_ROOM: str, RESIDENTS: int, BEDBUG: int, WARNING: int):
+def add_room(db, ROOM_ID: int, NUMBER1: int, TYPE_ROOM: str, RESIDENTS: int,des:str, BEDBUG: int, WARNING: int):
     try:
         with db.cursor() as cursor:
-            date1: str = '2020-05-31 19:23:55'
+            date1: str = '2020-05-31'
+            date1: str = des #29.05.2021
             sql = """INSERT INTO awm.room(ROOM_ID, "number", TYPE_ROOM, RESIDENTS, DISINFECTION, BEDBUG, WARNING)  """ \
-                  """VALUES (:ROOM_ID, :NUMBER1, :TYPE_ROOM, :RESIDENTS, to_date(:date1, 'YYYY-MM-DD HH24:MI:SS'), :BEDBUG, :WARNING)"""
+                  """VALUES (:ROOM_ID, :NUMBER1, :TYPE_ROOM, :RESIDENTS, to_date(:date1, 'DD-MM-YYYY'), :BEDBUG, :WARNING)"""
             cursor.execute(sql, [ROOM_ID, NUMBER1, TYPE_ROOM, RESIDENTS, date1, BEDBUG, WARNING])
             db.commit()
             print("done")
@@ -96,21 +76,26 @@ def add_room(db, ROOM_ID: int, NUMBER1: int, TYPE_ROOM: str, RESIDENTS: int, BED
 # VALUES (2, 11440149, 0, 'adas', 1777, 'dsfs', TO_DATE('2021-06-01 19:08:54', 'YYYY-MM-DD HH24:MI:SS'),
 #         TO_DATE('2021-06-01 19:08:55', 'YYYY-MM-DD HH24:MI:SS'), 1, 1);
 
-
-
-def add_stydent(db, STUDENT_HOSTEL_ID: int, STUDENT_ID: int, privileg: int, TYPE_OF_TRAINING, PAYMENT, VISIT,
-                PERIOD_FROM: str, PERIOD_TO: str, ROOM_ID: int, HOSTEL_ID: int):
+def add_stydent(db, STUDENT_HOSTEL_ID: int, STUDENT_ID: int, privil: bool, TYPE_OF_TRAINING, PAYMENT, VISIT,
+                PERIOD:str, ROOM_ID: int, HOSTEL_ID: int):
     try:
         with db.cursor() as cursor:
+            date = PERIOD.split('-')
+            date1=date[0]
+            date2=date[1]
+            if privil==True:
+                privileg=1
+            else:
+                privileg=0
             sql = """INSERT INTO awm.student_hostel(STUDENT_HOSTEL_ID, STUDENT_ID, "privileges", TYPE_OF_TRAINING, PAYMENT, VISIT,"""\
                                """ PERIOD_FROM, PERIOD_TO, ROOM_ID, HOSTEL_ID)  """ \
                   """VALUES (:STUDENT_HOSTEL_ID, :STUDENT_ID, :privileg, :TYPE_OF_TRAINING, :PAYMENT, :VISIT,"""\
-                               """ TO_DATE(:PERIOD_FROM, 'YYYY-MM-DD HH24:MI:SS'),"""\
-        """ TO_DATE(:PERIOD_TO, 'YYYY-MM-DD HH24:MI:SS'), :ROOM_ID, :HOSTEL_ID)"""
+                               """ to_date(:date1, 'DD-MM-YYYY'),"""\
+        """ to_date(:date2, 'DD-MM-YYYY'), :ROOM_ID, :HOSTEL_ID)"""
             cursor.execute(sql, [STUDENT_HOSTEL_ID, STUDENT_ID, privileg, TYPE_OF_TRAINING, PAYMENT, VISIT,
-                                 PERIOD_FROM, PERIOD_TO, ROOM_ID, HOSTEL_ID])
+                                 date1, date2, ROOM_ID, HOSTEL_ID])
             db.commit()
-            print("done")
+            # print("done")
         return True
     except Exception as e:
         print(e)
@@ -118,45 +103,43 @@ def add_stydent(db, STUDENT_HOSTEL_ID: int, STUDENT_ID: int, privileg: int, TYPE
 
 
 if __name__ == "__main__":
-    # client = MongoClient()
-    # db = client['MongoDB']
-    # roomers = db['Studens']
-    #
+
     connection = cx_Oracle.connect(
         "sys",
         "oracle",
         "itmo.fatalist.tech:1522/xe",
         cx_Oracle.SYSDBA
     )
+    client = MongoClient()
+    db = client['MongoDB']
+    roomers = db['students_hostel']
 
-    # add_hostel(connection,3,"общежитие 3",180)
-    # add_room(connection, 34, 23, "3 chil", 2, 1, 1)
-    add_stydent(db=connection,STUDENT_HOSTEL_ID=3, STUDENT_ID=11440017, privileg=0,
-                TYPE_OF_TRAINING='плати55', PAYMENT=1700, VISIT='вышел', PERIOD_FROM='2021-06-01 18:37:00',
-                PERIOD_TO='2021-06-01 18:37:03', ROOM_ID=1, HOSTEL_ID=1)
+    """Заполнение общежитий"""
+    hostel = {}
+    for i in roomers.find():
+        hostel[i["Общежитие"]["hostel_id"]]=[i["Общежитие"]["Местоположение"],i["Общежитие"]["Количество комнат в здании"]]
+    for key,value in hostel.items():
+        add_hostel(connection, key, value[0], value[1])
+    """Заполнение комнат"""
+    rooms = {}
+    for i in roomers.find():
+        rooms[i["Комната"]["room_id"]]=[i["Комната"]["Номер комнаты"],i["Комната"]["Тип комнаты"],i["Комната"]["Проживающих"],
+                                        i["Комната"]["Когда проводили дезинфекцию дата"],i["Комната"]["Клопы"],i["Комната"]["Предупреждения"]]
+    for key,value in rooms.items():
+        add_room(connection, key, value[0], value[1], value[2], value[3], value[4],value[5])
+    #     print(connection, key, value)
 
-    file = "STUDENT.csv"
-    home = 1
-    nomer = 1
-    floop = 1
-    data = []
-    i = 1
-    # with open(file, newline='', encoding='utf-8') as f:
-    #     reader = csv.DictReader(f,delimiter=',')
-    #     for row in reader:
-    #         print(row["STUDENT_ID"])
 
-    # for row in reader:
-    #     dat=add_student(int(row["\ufeffperson_id"]),row["name"],home,nomer,floop)
-    #     i+=1
-    #     if i%3==0:
-    #         nomer+=1
-    #         i=0
-    #     if nomer%15==0:
-    #         floop+=1
-    #         nomer=0
-    #     if floop%11==0:
-    #         home+=1
-    #         floop=0
-    #         nomer=0
-    #     roomers.insert(dat)
+    """Заполнение студентов"""
+    j=1
+    data=[]
+    for i in tqdm(roomers.find()):
+        data.append(i)
+    for i in tqdm(data):
+        add_stydent(db=connection, STUDENT_HOSTEL_ID=j, STUDENT_ID=i["Идентификатор"], privil=i["Льготы"],
+                    TYPE_OF_TRAINING=i["Вид обучения"], PAYMENT=i["Сумма оплаты"], VISIT=i["Период посещения"],
+                    PERIOD=i["Период с/по"], ROOM_ID=i["Комната"]["room_id"], HOSTEL_ID=i["Общежитие"]["hostel_id"])
+        j+=1
+
+
+
